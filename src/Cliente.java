b08815d6fb2297c1b1ec667ff79593bc55248d34
João Mira <jmirauniversitario@gmail.com>
2025-04-20 20:21:42 -0300

    Definindo opções do cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,8 +1,12 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Cliente {
     public static void main(String [] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean ativo = true;
+
         try (Socket socket = new Socket("localhost", 12345);
              BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
@@ -14,9 +18,9 @@ public class Cliente {
 
             String action = choice.equals("1") ? "LOGIN" : "REGISTER";
 
-            System.out.print("Usuário: ");
+            System.out.print("Insira o seu nome de usuário: ");
             String username = console.readLine();
-            System.out.print("Senha: ");
+            System.out.print("Insira a sua senha: ");
             String password = console.readLine();
 
             out.println(action);
@@ -24,12 +28,38 @@ public class Cliente {
             out.println(password);
 
             String response = in.readLine();
-            System.out.println("Resposta do servidor: " + response);
 
-            // Recebe a listagem de arquivos
-            String line;
-            while ((line = in.readLine()) != null && !line.equals("FIM_LISTA")) {
-                System.out.println(line);
+            switch (response) {
+                case "Registro efetuado com sucesso!" -> {
+                    System.out.println(response+ " Seus diretórios estão sendo criados.");
+                }
+                case "Falha no registro. Tente novamente." -> {
+                    System.out.println(response);
+                }
+            }
+
+            System.out.println("\nBem-vindo(a), "+username+"!");
+            int opcao;
+            System.out.println("Escolha uma opção:\n1 - Listar arquvios\n2 - Inserir arquivo\n3 - Baixar arquivos\n4 " +
+                    "- Sair");
+            opcao = scanner.nextInt();
+            switch(opcao) {
+                case 1 -> {
+                    System.out.println("[listagem]");
+                    String line;
+                    while ((line = in.readLine()) != null && !line.equals("FIM_LISTA")) {
+                        System.out.println(line);
+                    }
+                }
+                case 2 -> {
+                    System.out.println("[inserção]");
+                }
+                case 3 -> {
+                    System.out.println("[baixar]");
+                }
+                case 4 -> {
+                    System.out.println("[sair]");
+                }
             }
 
         } catch (IOException e) {
